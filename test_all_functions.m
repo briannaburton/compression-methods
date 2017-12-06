f3f0142dca98689132647e0916179d569129702d
doiriel <dono.vanegas@gmail.com>
2017-12-06 15:50:08 +0100

    add huffman test and names for functions output

--- test_all_functions.m
+++ test_all_functions.m
@@ -1,3 +1,6 @@
+% Image Compression Lab
+% Brianna BURTON, Doiriel VANEGAS
+
 %Clear command line
 clc;
 %Closing all open windows
@@ -9,18 +12,25 @@ clear all;
 %Arithmetic coding. Consider the DNA alphabet {A,C,T,G} with the respective
 %probabilities {0.5,0.3,0.15,0.05}. Encode using the aritmetic_encoding
 %function and output the binary sequence
-arithmetic_encoding([1,2,3,4], [0.5,0.3,0.15,0.05], [1,2,3,1,4,2]) % {A,C,T,A,G,C};
+DNA_encoding = arithmetic_encoding([1,2,3,4], [0.5,0.3,0.15,0.05], [1,2,3,1,4,2]) % {A,C,T,A,G,C};
 
 %Generate the arithmetic coding for "BE_A_BE"
-arithmetic_encoding([1,2,3,4], [0.375,0.25,0.25,0.125], [2,1,3,4,3,2,1,1])
+BE_A_BE_encoding = arithmetic_encoding([1,2,3,4], [0.375,0.25,0.25,0.125], [2,1,3,4,3,2,1,1])
 
 %Check the solutions using the arithmetic_decoding function, using the
 %last entry in the binary code sequence
-arithmetic_decoding([1,2,3,4], [0.5,0.3,0.15,0.05], 0.6188625000000, 6) % {A,C,T,G}
-arithmetic_decoding([1,2,3,4], [0.375,0.25,0.25,0.125], 0.543766975402832, 8) % {E,B,_,A}
+DNA_decoding = arithmetic_decoding([1,2,3,4], [0.5,0.3,0.15,0.05], 0.6188625000000, 6) % {A,C,T,G}
+BE_A_BE_decoding = arithmetic_decoding([1,2,3,4], [0.375,0.25,0.25,0.125], 0.543766975402832, 8) % {E,B,_,A}
 
 %Exercise 2
+%1. Huffman encoding of a probability vector
+alphabet = ['A', 'C', 'T', 'G'];
+prob_vector = [0.5, 0.3, 0.15, 0.05];
+codeBook_vector = huffman_encoding(prob_vector,alphabet)
 
+%2. Huffman encoding of a gray image
+image = imread('cameraman.tif');
+codeBook_image = huffman_image(image)
 
 %Exercise 3 JPEG Encoder
 array = rgb2gray(imresize(imread('DSC_0004.JPG'),0.25));
